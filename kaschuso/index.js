611b17d991acf9e5aceb20fc96af51224964d340
Alim Weber <alim.weber@outlook.com>
2024-02-26 23:47:57 +0100

    kaschuso

--- kaschuso/index.js
+++ kaschuso/index.js
@@ -3,7 +3,8 @@ const cheerio = require("cheerio");
 const fs = require("fs")
 
 async function getValidatedSession({username, password}){
-	
+
+
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 
@@ -21,20 +22,17 @@ async function getValidatedSession({username, password}){
 		await page.waitForSelector(".mdl-layout__container", {
 			timeout:4000
 		})
-
+		
+	
 
 		let Cookie = (convert(await page.cookies()))
-
-
-		return {
-			err:false,
-			Cookie
-		}
+		
+		await browser.close()
+		
+		return Cookie
 	}catch(e){
 
-		return {
-			err:"no such user apparently"
-		}
+		throw new Error("Username or password are wrong!")	
 
 	}
 
@@ -52,29 +50,46 @@ function convert(puppeteerCookies) {
 }
 
 
+async function getGrades(session){
+	
+	let prefetch = await fetch("https://kaschuso.so.ch/ksso/index.php?pageid=21311&id=18d943c9e1ef5486&transid=896b31", {
+		headers:{
+			Cookie:session
+		}
+	})
+
+	let newSesh = prefetch.headers.get("set-cookie").split(";")[0]
+
+
+
+	session = session.replace(/PHPSESSID=.*;/g, newSesh + ";")
+
+
+	prefetch = await prefetch.text()
+	
+
+
+
+	let gradeurl = "https://kaschuso.so.ch/ksso/" + [...prefetch.match(/"[^"]*pageid=21311[^"]*"/g)][0].replaceAll(`"`, "").replaceAll("&amp;", "&")
+
+	console.log(gradeurl)
+
+	let gradePage = await fetch(gradeurl, {
+		Cookie:session,
+		Referer:"https://kaschuso.so.ch/ksso/loginto.php?pageid=21311&mode=4&lang="
+	}).then(res => res.text())
+	
+	fs.writeFileSync(__dirname + "/file.html", gradePage)
+
+}
+
 (async () => {
 
-	const Cookie = await getValidatedSession({username:"username", password:"password"}).then(res => {
+	const Cookie = await getValidatedSession({username:"alim.weber", password:"Venawa34puwa&"}).catch(console.log)
 
-		if(res.err){
-			throw new Error(res.err)
-		} else {
-			return res.Cookie
-		}
-	})
+	console.log(Cookie)
 
+	await getGrades(Cookie)	
 
-
-	let getpage = await fetch("https://kaschuso.so.ch/ksso/loginto.php?mode=0&lang=", {
-		
-		headers:{
-			Cookie
-		}
-
-	})
-
-	let text = (await getpage.text())
-
-
-
+	//getGrades(Cookie)
 })()
