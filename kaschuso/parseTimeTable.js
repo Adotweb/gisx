c7ba381f9deb3c982cce83c4d97c88461d7a570a
Alim <alim.weber@outlook.com>
2024-03-06 09:03:28 +0100

    parsetimetable

--- kaschuso/parseTimeTable.js
+++ kaschuso/parseTimeTable.js
@@ -9,8 +9,32 @@ let $ = cheerio.load(page)
 let table = $(".dhx_cal_data")
 
 
+let weekdays = [0, 1, 2, 3, 4].map(s => table.find(`div[data-column-index="${s}"]`).toArray().map(s => cheerio.load(s))[0])
+
+	.map((column,i) => {
+
+		let events = column(".dhx_cal_event").toArray().map(s => cheerio.load(s)).map(event => {
+
+			let eventbody = event(".stpt_event_body")
+
+
+
+			console.log(eventbody.attr(), i)
+		})
+			//.find(".stpt_event_body")
+
+
+
+
+		console.log(events)
+	})
+
+
+		
+
+
 let events = table.find(".dhx_cal_event").toArray().map(s => cheerio.load(s))
-	.map(event => {
+	.map((event, i) => {
 
 		let eventbody = event(".stpt_event_body")
 
@@ -35,18 +59,34 @@ let events = table.find(".dhx_cal_event").toArray().map(s => cheerio.load(s))
 		try{
 			classes = classes.split(",")
 		}catch(e){}
-
+	
+		if(!teacher){
+			classes = []
+		}
+		
+		teacher = rest.split(";")[1].split(":")[1].trim()
 
 		let LessonObject = {
 			time,
 			room,
 			coursename,
 			classes,
-			teacher
+			teacher,
+			index: i
 		}	
 
 		return LessonObject
 	})
 
 
-console.log(events)
+
+let bytime = [
+
+]
+
+
+//events = events.filter(s => s.time == "07:35 - 08:20")
+
+
+
+
